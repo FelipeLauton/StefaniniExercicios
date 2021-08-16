24ef5c6a86b39dd90cbf7ac7718b37015e2f0413
Felipe Lauton <felipelauton021@gmail.com>
2021-08-16 15:38:09 -0300

    exercicio 1

---
+++ projetoprova/src/br/com/stefanini/projetoprova/model/Prova.java
@@ -0,0 +1,46 @@
+package br.com.stefanini.projetoprova.model;
+
+public class Prova {
+
+	private Integer idProva;
+	private String nomeProva;
+	private int nota;
+	
+	public Prova() {
+	}
+
+	public Prova(Integer idProva, String nomeProva) {
+		super();
+		this.idProva = idProva;
+		this.nomeProva = nomeProva;
+	}
+
+	@Override
+	public String toString() {
+		return "Prova [idProva=" + idProva + ", nomeProva=" + nomeProva + ", nota=" + nota + "]";
+	}
+
+	public Integer getIdProva() {
+		return idProva;
+	}
+
+	public void setIdProva(Integer idProva) {
+		this.idProva = idProva;
+	}
+
+	public String getNomeProva() {
+		return nomeProva;
+	}
+
+	public void setNomeProva(String nomeProva) {
+		this.nomeProva = nomeProva;
+	}
+
+	public int getNota() {
+		return nota;
+	}
+
+	public void setNota(int nota) {
+		this.nota = nota;
+	}
+}
